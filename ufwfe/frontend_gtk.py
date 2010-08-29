ea95e467b21ff85a4401ad2a62e99a45ee3ce43c
Darwin Bautista <daruuin@gmail.com>
2010-08-29 07:56:52 +0000

    * Make extensive use of GtkActions and GtkActionGroups * Revamped menubar and main window toolbar

--- ufwfe/frontend_gtk.py
+++ ufwfe/frontend_gtk.py
@@ -45,6 +45,7 @@ class GtkFrontend(Frontend):
         self.about_dialog = self.ui.get_object('about_dialog')
         self._init_dialogs()
         self._init_main_window()
+        self._init_actions()
         # connect signals and show main window
         self.ui.connect_signals(self)
         main_window = self.ui.get_object('main_window')
@@ -63,8 +64,8 @@ class GtkFrontend(Frontend):
 
     def _init_prefs_dialog(self):
         # comboboxes
-        logging_opts = map(str.lower, self._get_cbox_values('logging_cbox'))
-        policy_opts = map(str.lower, self._get_cbox_values('incoming_policy_cbox'))
+        logging_opts = map(str.lower, self._get_combobox_values('logging_cbox'))
+        policy_opts = map(str.lower, self._get_combobox_values('incoming_policy_cbox'))
         logging = logging_opts.index(self.backend.defaults['loglevel'])
         default_incoming = policy_opts.index(self.backend.get_default_policy('input'))
         default_outgoing = policy_opts.index(self.backend.get_default_policy('output'))
@@ -81,18 +82,43 @@ class GtkFrontend(Frontend):
         conf = self.backend.defaults['ipv6']
         cb.set_active(conf == 'yes')
 
-    def _set_toggle_state(self, active):
-        label = ('Disable Firewall' if active else 'Enable Firewall')
-        img_id = ('stop' if active else 'play')
-        toggle = self.ui.get_object('toggle_firewall')
-        toggle.set_label(_(label))
-        toggle.set_active(active)
-        img = self.ui.get_object(img_id)
-        toggle.set_image(img)
-        for name in ('insert_rule', 'delete_rule', 'rule_up', 'rule_down'):
-            self.ui.get_object(name).set_sensitive(active)
+    def _init_actions(self):
+        groups = {
+            'rule_actions': [
+                'rule_add', 'rule_delete', 'rule_edit',
+                'rule_up', 'rule_down'
+            ],
+            'firewall_actions': [
+                'firewall_reload', 'firewall_reset', 'firewall_update'
+            ]
+        }
+        for group, actions in groups.iteritems():
+            ag = self.ui.get_object(group)
+            for action in actions:
+                a = self.ui.get_object(action)
+                ag.add_action(a)
 
-    def _get_cbox_values(self, name):
+    def _set_toggle_state(self, active):
+        if active:
+            label = 'Disable'
+            short_label = 'Disable Firewall'
+            stock_id = gtk.STOCK_STOP
+        else:
+            label = 'Enable'
+            short_label = 'Enable Firewall'
+            stock_id = gtk.STOCK_MEDIA_PLAY
+        # Set action properties
+        action = self.ui.get_object('firewall_toggle')
+        action.set_active(active)
+        action.set_label(_(label))
+        action.set_short_label(_(short_label))
+        action.set_stock_id(stock_id)
+        # Enable/disable related controls
+        self.ui.get_object('rules_view').set_sensitive(active)
+        self.ui.get_object('rule_actions').set_sensitive(active)
+        self.ui.get_object('firewall_actions').set_sensitive(active)
+
+    def _get_combobox_values(self, name):
         model = self.ui.get_object(name).get_model()
         values = []
         for v in model:
@@ -107,7 +133,7 @@ class GtkFrontend(Frontend):
             return model[active][0]
 
     def _set_combobox_value(self, name, value):
-        values = map(str.lower, self._get_cbox_values(name))
+        values = map(str.lower, self._get_combobox_values(name))
         i = values.index(value.lower())
         cbox = self.ui.get_object(name)
         cbox.set_active(i)
@@ -258,6 +284,175 @@ class GtkFrontend(Frontend):
             self.ui.get_object('dst_port_custom_rbutton').set_active(True)
             self.ui.get_object('dst_port_custom_entry').set_text(rule.dport)
 
+    def _get_selected_rule_pos(self):
+        model, itr = self.selection.get_selected()
+        if not itr:
+            return 0
+        return model.get_path(itr)[0] + 1
+
+    # ---------------------- Application Actions -----------------------
+
+    def on_rules_export_activate(self, action):
+        pass
+
+    def on_rules_import_activate(self, action):
+        pass
+
+    def on_quit_activate(self, action):
+        gtk.main_quit()
+
+    def on_prefs_dialog_show_activate(self, widget):
+        if self.prefs_dialog.run():
+            # loglevel
+            level = self._get_combobox_value('logging_cbox').lower()
+            self.set_loglevel(level)
+            # default incoming
+            policy = self._get_combobox_value('incoming_policy_cbox').lower()
+            self.backend.set_default_policy(policy, 'incoming')
+            # default outgoing
+            policy = self._get_combobox_value('outgoing_policy_cbox').lower()
+            self.backend.set_default_policy(policy, 'outgoing')
+            # enable IPv6?
+            cb = self.ui.get_object('enable_ipv6')
+            self.enable_ipv6(cb.get_active())
+            # reload firewall
+            self.reload()
+            self._set_statusbar_text(_('Preferences saved'))
+        self.prefs_dialog.hide()
+
+    def on_about_dialog_show_activate(self, widget):
+        self.about_dialog.run()
+        self.about_dialog.hide()
+
+    # ------------------------ Firewall Actions ------------------------
+
+    def on_firewall_toggle_toggled(self, action):
+        if self.backend._is_enabled():
+            res = self.set_enabled(False)
+            self._set_statusbar_text(res)
+            self._set_toggle_state(False)
+        else:
+            res = self.set_enabled(True)
+            self._set_statusbar_text(res)
+            self._set_toggle_state(True)
+
+    def on_firewall_reload_activate(self, action):
+        if self.reload():
+            self._update_rules_model()
+
+    def on_firewall_reset_activate(self, action):
+        msg = _('Resetting all rules to installed defaults.\nProceed with operation?')
+        res = self._show_dialog(msg, msg_type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_YES_NO)
+        if res == gtk.RESPONSE_YES:
+            self.reset(True)
+            self.rules_model.clear()
+            self._set_toggle_state(False)
+            self._set_statusbar_text(_('Firewall defaults restored'))
+
+    def on_firewall_update_activate(self, action):
+        res = self.application_update('all')
+        if not res:
+            res = _('Nothing to update')
+        self._show_dialog(res, msg_type=gtk.MESSAGE_INFO)
+
+    # -------------------------- Rule Actions --------------------------
+
+    def on_rule_add_activate(self, action):
+        if not self.backend._is_enabled():
+            return
+        self._restore_rule_dialog_defaults()
+        while True:
+            if self.rule_dialog.run():
+                try:
+                    rule = self._get_rule_from_dialog()
+                except UFWError, e:
+                    self._show_dialog(e.value, 'rule_dialog')
+                    continue
+                try:
+                    res = self.set_rule(rule)
+                except UFWError, e:
+                    self._show_dialog(e.value, 'rule_dialog')
+                    continue
+                self._set_statusbar_text(res)
+                self._update_rules_model()
+            break
+        self.rule_dialog.hide()
+
+    def on_rule_edit_activate(self, action):
+        if not self.backend._is_enabled():
+            return
+        pos = self._get_selected_rule_pos()
+        if not pos:
+            return
+        rules = self.backend.get_rules()
+        i = self.rules_model[pos - 1][8]
+        self._load_rule_to_dialog(rules[i])
+        while True:
+            if self.rule_dialog.run():
+                try:
+                    rule = self._get_rule_from_dialog()
+                except UFWError, e:
+                    self._show_dialog(e.value, 'rule_dialog')
+                    continue
+                try:
+                    self.update_rule(pos, rule)
+                except UFWError, e:
+                    self._show_dialog(e.value, 'rule_dialog')
+                    continue
+                self._set_statusbar_text(_('Rule updated'))
+                self._update_rules_model()
+                self.selection.select_path(pos - 1)
+            break
+        self.rule_dialog.hide()
+
+    def on_rule_delete_activate(self, action):
+        if not self.backend._is_enabled():
+            return
+        pos = self._get_selected_rule_pos()
+        if not pos:
+            return
+        try:
+            res = self.delete_rule(pos, True)
+        except UFWError, e:
+            self._show_dialog(e.value)
+        else:
+            self._set_statusbar_text(res)
+            self._update_rules_model()
+
+    def on_rule_up_activate(self, action):
+        if not self.backend._is_enabled():
+            return
+        pos = self._get_selected_rule_pos()
+        if not pos:
+            return
+        new = pos - 1
+        if new < 1:
+            return
+        self.move_rule(pos, new)
+        self._update_rules_model()
+        self.selection.select_path(new - 1)
+
+    def on_rule_down_activate(self, action):
+        if not self.backend._is_enabled():
+            return
+        pos = self._get_selected_rule_pos()
+        if not pos:
+            return
+        new = pos + 1
+        if new > len(self.rules_model):
+            return
+        self.move_rule(pos, new)
+        self._update_rules_model()
+        self.selection.select_path(new - 1)
+
+    # ------------------------ Other Callbacks -------------------------
+
+    def on_main_window_destroy(self, widget):
+        self.ui.get_object('quit').activate()
+
+    def on_rules_view_row_activated(self, widget, path, view_column):
+        self.ui.get_object('rule_edit').activate()
+
     def on_src_addr_custom_rbutton_toggled(self, widget):
         entry = self.ui.get_object('src_addr_custom_entry')
         entry.set_sensitive(widget.get_active())
@@ -287,146 +482,6 @@ class GtkFrontend(Frontend):
         app_rbutton = self.ui.get_object('src_app_rbutton')
         protocol_cbox = self.ui.get_object('protocol_cbox')
         protocol_cbox.set_sensitive(not widget.get_active() and not app_rbutton.get_active())
-
-    def on_toggle_firewall_toggled(self, widget):
-        if self.backend._is_enabled():
-            res = self.set_enabled(False)
-            self._set_statusbar_text(res)
-            self._set_toggle_state(False)
-        else:
-            res = self.set_enabled(True)
-            self._set_statusbar_text(res)
-            self._set_toggle_state(True)
-
-    def on_reload_firewall_clicked(self, widget):
-        if self.reload():
-            self._update_rules_model()
-
-    def on_insert_rule_clicked(self, widget):
-        if self.backend._is_enabled():
-            self._restore_rule_dialog_defaults()
-            while True:
-                if self.rule_dialog.run():
-                    try:
-                        rule = self._get_rule_from_dialog()
-                    except UFWError, e:
-                        self._show_dialog(e.value, 'rule_dialog')
-                        continue
-                    try:
-                        res = self.set_rule(rule)
-                    except UFWError, e:
-                        self._show_dialog(e.value, 'rule_dialog')
-                        continue
-                    self._set_statusbar_text(res)
-                    self._update_rules_model()
-                break
-            self.rule_dialog.hide()
-
-    def on_delete_rule_clicked(self, widget):
-        if self.backend._is_enabled():
-            model, itr = self.selection.get_selected()
-            if not itr:
-                return
-            pos = model.get_path(itr)[0] + 1
-            try:
-                res = self.delete_rule(pos, True)
-            except UFWError, e:
-                self._show_dialog(e.value)
-            else:
-                self._set_statusbar_text(res)
-                self._update_rules_model()
-
-    def on_rule_up_clicked(self, widget):
-        if self.backend._is_enabled():
-            model, itr = self.selection.get_selected()
-            if not itr:
-                return
-            pos = model.get_path(itr)[0] + 1
-            new = pos - 1
-            if new < 1:
-                return
-            self.move_rule(pos, new)
-            self._update_rules_model()
-            self.selection.select_path(new - 1)
-
-    def on_rule_down_clicked(self, widget):
-        if self.backend._is_enabled():
-            model, itr = self.selection.get_selected()
-            if not itr:
-                return
-            pos = model.get_path(itr)[0] + 1
-            new = pos + 1
-            if new > len(self.rules_model):
-                return
-            self.move_rule(pos, new)
-            self._update_rules_model()
-            self.selection.select_path(new - 1)
-
-    def on_help_menu_activate(self, widget):
-        self.about_dialog.run()
-        self.about_dialog.hide()
-
-    def on_rules_view_row_activated(self, widget, path, view_column):
-        if not self.backend._is_enabled():
-            return
-        pos = path[0] + 1
-        rules = self.backend.get_rules()
-        i = self.rules_model[pos - 1][8]
-        self._load_rule_to_dialog(rules[i])
-        while True:
-            if self.rule_dialog.run():
-                try:
-                    rule = self._get_rule_from_dialog()
-                except UFWError, e:
-                    self._show_dialog(e.value, 'rule_dialog')
-                    continue
-                try:
-                    self.update_rule(pos, rule)
-                except UFWError, e:
-                    self._show_dialog(e.value, 'rule_dialog')
-                    continue
-                self._set_statusbar_text(_('Rule updated'))
-                self._update_rules_model()
-                self.selection.select_path(pos - 1)
-            break
-        self.rule_dialog.hide()
-
-    def on_reset_fw_menu_activate(self, widget):
-        msg = _('Resetting all rules to installed defaults.\nProceed with operation?')
-        res = self._show_dialog(msg, msg_type=gtk.MESSAGE_WARNING, buttons=gtk.BUTTONS_YES_NO)
-        if res == gtk.RESPONSE_YES:
-            self.reset(True)
-            self.rules_model.clear()
-            self._set_toggle_state(False)
-            self._set_statusbar_text(_('Firewall defaults restored'))
-
-    def on_update_fw_menu_activate(self, widget):
-        res = self.application_update('all')
-        if not res:
-            res = _('Nothing to update')
-        self._show_dialog(res, msg_type=gtk.MESSAGE_INFO)
-
-    def on_prefs_menu_activate(self, widget):
-        if self.prefs_dialog.run():
-            # loglevel
-            level = self._get_combobox_value('logging_cbox').lower()
-            self.set_loglevel(level)
-            # default incoming
-            policy = self._get_combobox_value('incoming_policy_cbox').lower()
-            self.backend.set_default_policy(policy, 'incoming')
-            # default outgoing
-            policy = self._get_combobox_value('outgoing_policy_cbox').lower()
-            self.backend.set_default_policy(policy, 'outgoing')
-            # enable IPv6?
-            cb = self.ui.get_object('enable_ipv6')
-            self.enable_ipv6(cb.get_active())
-            # reload firewall
-            self.reload()
-            self._set_statusbar_text(_('Preferences saved'))
-        self.prefs_dialog.hide()
-
-    def on_main_window_destroy(self, widget):
-        gtk.main_quit()
 
 
 def main():
